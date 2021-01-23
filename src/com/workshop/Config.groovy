851ecf05abc3d81e4540e6f6d19c0f216daa8917
Andika Ahmad Ramadhan <andikahmadr@gmail.com>
2021-01-23 15:55:18 +0700

    Add build pipeline

--- src/com/workshop/Config.groovy
+++ src/com/workshop/Config.groovy
@@ -3,9 +3,10 @@ package com.workshop
 
 class Config {
     // Place Default Variable Here
+    def default_docker_registry = "https://registry-1.docker.io/v2/"
+    def default_docker_registry_jenkins_cred = 'cred-docker'
     // Docker related default variable
     def default_docker_jenkins_tool = 'docker'
-
    // Golang related default variable
     def default_golang_base_image = "tobapramudia/tkpd-demo:onbuild"
 
